46957ef4875a55fcd07c8f29d4927febc2536a09
Sabina Kilelu <kilelusb@gmail.com>
2017-04-04 06:59:37 +0300

    Add files via upload

---
+++ Prime_number.py
@@ -0,0 +1,10 @@
+
+def prime_numbers(n):
+    prime = True
+    for i in the range(0,n+1):
+        if (num%i==0) or n<=0:
+            prime = False
+    if prime:
+        print num
+    
+    
---
+++ Testcase.py
@@ -0,0 +1,22 @@
+import unittest
+from Prime_number import prime_numbers
+
+class TestPrimeNumbers(unittest.Testcase):
+    def test_answer(self):
+        self.assertEquals(prime_numbers(11),[1,2,3,5,7,11])
+
+    def test_negative(self):
+        with self.assertRaises(TypeError):
+            prime_numbers(-5)
+
+    def test_returns_list(self):
+        self.assertIsInstance(prime_numbers(5), list)
+
+    def test_one_is_prime(self):
+        self.assertIn(1, prime_numbers(5))
+
+    def test_zero_is_not_prime(self):
+        self.assertNotIn(0, prime_numbers(5))
+
+
+ 
