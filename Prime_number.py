1d1173816dd12155551a26ed761d850e7c8344c4
Sabina Kilelu <kilelusb@gmail.com>
2017-04-04 07:15:50 +0300

    Fixed the syntax error in the Prime_number file

--- Prime_number.py
+++ Prime_number.py
@@ -1,10 +1,15 @@
 
 def prime_numbers(n):
     prime = True
-    for i in the range(0,n+1):
-        if (num%i==0) or n<=0:
+    if type(n) == int:
+        if n>=1:
+            for i in range(0,n+1):
+                if i%2==1:
+                    prime.append(i)
+                    return prime
+        elif (num%i==0) or n<=0:
             prime = False
     if prime:
-        print num
+        print (num)
     
     
