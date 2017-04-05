b1cb31fa45554826c90e5448b2c892665ca21790
Sabina Kilelu <kilelusb@gmail.com>
2017-04-05 07:57:27 +0300

    Update Prime_number.py
    Fixing the syntax errors that had occured

--- Prime_number.py
+++ Prime_number.py
@@ -1,15 +1,18 @@
 
 def prime_numbers(n):
-    prime = True
+    prime =[]
     if type(n) == int:
-        if n>=1:
-            for i in range(0,n+1):
-                if i%2==1:
+        if n>1:
+            not_prime = set(j for i in range(2,n+1) for j in range(i*2,n+1, i))
+            for i in range(2,n+1):
+                if i not in not_prime:
                     prime.append(i)
                     return prime
-        elif (num%i==0) or n<=0:
-            prime = False
-    if prime:
-        print (num)
+    elif type(n)==str:
+        return 'Not a prime number'
+    elif type(n)==list:
+        return 'Not a prime number'
+    else:
+        pass
     
     
