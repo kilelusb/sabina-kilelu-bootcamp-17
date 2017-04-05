ee8489e2d28b6262d03ecbbb22c6de82af3f5522
Sabina Kilelu <kilelusb@gmail.com>
2017-04-05 07:15:27 +0300

    Create word_count.py
    The results of the word count in a list will come in a dictionary

---
+++ Day 3/word_count.py
@@ -0,0 +1,7 @@
+def word_count(A):
+    wordlist = A.split()
+    word_freq = []
+    for word in wordlist:
+        wordlist.append(word)
+        word_freq ={i:wordlist.count(i) for i in wordlist}
+        return word_freq
