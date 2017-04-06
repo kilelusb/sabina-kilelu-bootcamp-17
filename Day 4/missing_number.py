cd6c13c95f3b5d01f714d3fd379230fef5681ed9
Sabina Kilelu <kilelusb@gmail.com>
2017-04-06 10:48:05 +0300

    Update to ensure all items are integers

--- Day 4/missing_number.py
+++ Day 4/missing_number.py
@@ -1,11 +1,16 @@
 def find_missing(A,B):
-    array1=set(A)
-    array2=set(B)
-    if (array1== array2) or array1==[] or array2==[]:
-        return 0
-    elif (len(array1)-len(array2))==1:
-        return list(array1-array2)[0]
-    elif (len(array2)-len(array1))==1:
-        return list(array2-array1)[0]
-    else:
-        return 'Missing number cannot be more than one'
+    #checking if the list contains only integers
+
+    AandB=A+B
+    if all(isinstance(item,int) for item in AandB):
+           array1=set(A)
+           array2=set(B)
+
+           if (array1== array2) or array1==[] or array2==[]:
+               return 0
+           elif (len(array1)-len(array2))==1:
+               return list(array1-array2)[0]
+           elif (len(array2)-len(array1))==1:
+               return list(array2-array1)[0]
+           else:
+               return 'Missing number cannot be more than one'
